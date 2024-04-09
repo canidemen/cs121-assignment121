43a3814270cf933c6c3dee321a136fa3f9c896ba
canidemen <cidemen@uci.edu>
2024-04-09 16:48:58 -0700

    sys args fix

--- PartB.py
+++ PartB.py
@@ -1,4 +1,5 @@
 import PartA
+import sys
 
 def tokenize_file(file_path):
     file_list = PartA.tokenize(file_path)
@@ -6,8 +7,8 @@ def tokenize_file(file_path):
 
 
 def compare_files():
-    file_1 = input("Please enter a path to a text file:\n")
-    file_2 = input("Please enter a path to a second file:\n")
+    file_1 = sys.argv[1]
+    file_2 = sys.argv[2]
     file_1_dict = tokenize_file(file_1)
     file_2_dict = tokenize_file(file_2)
     similar_words = []
